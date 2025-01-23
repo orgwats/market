878bbdfe7b0e6efd6c254861e2532d077fa61735
waik1ki <arbutus@outlook.kr>
2025-01-23 23:03:07 +0900

    refactor: wats 실행 아키텍쳐 수정

--- cmd/trader/main.go
+++ cmd/trader/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"wats/config"
 	"wats/internal/trader"
 )
@@ -9,5 +14,24 @@ func main() {
 	c := config.LoadConfig()
 	t := trader.NewTrader(c)
 
+	go func() {
+		listenForSignals()
+		log.Println("[main] Received OS signal, stopping trader...")
+
+		t.Stop()
+
+		time.Sleep(500 * time.Millisecond)
+		os.Exit(0)
+	}()
+
 	t.Start()
+
+	select {}
+}
+
+func listenForSignals() {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+
+	<-sigCh
 }
