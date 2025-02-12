17f5f15984fb46cbbce0064022768d584b443f0d
waik1ki <arbutus@outlook.kr>
2025-02-12 18:30:26 +0900

    refactor: CandleBuffer 로직 수정

--- internal/chart/candle/candle.go
+++ internal/chart/candle/candle.go
@@ -1,54 +1,39 @@
 package candle
 
 import (
-	"wats/internal/database"
-	s "wats/internal/stream"
 	"wats/internal/types"
 )
 
-type RingBuffer struct {
+type CandleBuffer struct {
 	buf  []*types.Candle
 	head int
 	size int
 }
 
-type CandleBuffer struct {
-	ch   chan *types.Candle
-	ring *RingBuffer
+func NewCandleBuffer(size int) *CandleBuffer {
+	return &CandleBuffer{
+		buf:  make([]*types.Candle, size),
+		head: 0,
+		size: size,
+	}
 }
 
-func NewCandleBuffer(db *database.Database, symbol string) *CandleBuffer {
-	cb := &CandleBuffer{
-		ch: s.NewStream(symbol),
-		ring: &RingBuffer{
-			buf:  make([]*types.Candle, 30),
-			head: 0,
-			size: 30,
-		},
-	}
-
-	for _, c := range db.GetCandles(symbol) {
-		// Candle ring buffer 초기화
+func (cb *CandleBuffer) Init(cds []*types.Candle) {
+	for _, c := range cds {
 		cb.AddCandle(c)
 	}
-
-	return cb
-}
-
-func (cb *CandleBuffer) ReceiveCandle() *types.Candle {
-	return <-cb.ch
 }
 
 func (cb *CandleBuffer) GetCandles() []*types.Candle {
-	return cb.ring.buf
+	return cb.buf
 }
 
 func (cb *CandleBuffer) AddCandle(c *types.Candle) {
-	cb.ring.buf[cb.ring.head] = c
-	cb.ring.head = (cb.ring.head + 1) % cb.ring.size
+	cb.buf[cb.head] = c
+	cb.head = (cb.head + 1) % cb.size
 }
 
 func (cb *CandleBuffer) UpdateLastCandle(c *types.Candle) {
-	idx := (cb.ring.head - 1 + cb.ring.size) % cb.ring.size
-	cb.ring.buf[idx] = c
+	idx := (cb.head - 1 + cb.size) % cb.size
+	cb.buf[idx] = c
 }
