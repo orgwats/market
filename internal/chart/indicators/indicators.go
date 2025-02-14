cbb9b380c7b963a6b113e400bead0063013a5f7b
waik1ki <arbutus@outlook.kr>
2025-02-14 23:15:17 +0900

    refactor: Indicator 로직 수정

--- internal/chart/indicators/indicators.go
+++ internal/chart/indicators/indicators.go
@@ -1,29 +1,17 @@
 package indicators
 
-import (
-	"log"
-	"wats/internal/chart/candle"
-)
+import "wats/internal/types"
 
 type Indicators struct {
 	RSI chan float64
 }
 
-func NewIndicators(cb *candle.CandleBuffer) *Indicators {
-	i := &Indicators{
+func NewIndicators() *Indicators {
+	return &Indicators{
 		RSI: make(chan float64),
 	}
+}
 
-	go func() {
-		for {
-			c := cb.ReceiveCandle()
-			log.Println(c)
-			// 첫 스트림 데이터를 추가해주기 위해 once.Do 필요
-
-			cb.AddCandle(c)
-			log.Println(c, CalculateRSI(cb.GetCandles(), 14))
-		}
-	}()
-
-	return i
+func (i *Indicators) Update(candles []*types.Candle) {
+	i.RSI <- CalculateRSI(candles, 14)
 }
