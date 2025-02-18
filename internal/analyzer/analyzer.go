5f3cdb2af7be24887bf73fba22ce540727203720
waik1ki <arbutus@outlook.kr>
2025-02-18 23:46:17 +0900

    refactor: Analyzer 구조체 및 생성 로직 수정

--- internal/analyzer/analyzer.go
+++ internal/analyzer/analyzer.go
@@ -2,33 +2,22 @@ package analyzer
 
 import (
 	"context"
-	"log"
-	"wats/internal/types"
+	"wats/internal/chart"
 )
 
 type Analyzer struct {
 	ctx context.Context
 
-	channel *types.TraderChannel
+	chart *chart.Chart
 }
 
-func NewAnalyzer(ctx context.Context, ch *types.TraderChannel) *Analyzer {
+func NewAnalyzer(ctx context.Context, chart *chart.Chart) *Analyzer {
 	return &Analyzer{
 		ctx: ctx,
 
-		channel: ch,
+		chart: chart,
 	}
 }
 
 func (a *Analyzer) Strat() {
-	// TODO: 임시 선언 > 추후 설정에서 불러오는 방식으로 수정
-	symbols := map[string]bool{
-		"TRUMPUSDT": true,
-	}
-
-	for e := range a.channel.Stream {
-		for k := range symbols {
-			log.Printf("symbol: %s, price: %s", e[k].Symbol, e[k].MarkPrice)
-		}
-	}
 }
