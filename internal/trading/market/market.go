9fd356e5076c9559cb49414a5c147e99c3c1f8d3
waik1ki <arbutus@outlook.kr>
2025-02-23 20:49:53 +0900

    refactor: 바이낸스 마켓 데이터 다운로드 로직 수정

--- internal/trading/market/market.go
+++ internal/trading/market/market.go
@@ -2,13 +2,11 @@ package market
 
 import (
 	"archive/zip"
-	"encoding/csv"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"strconv"
-	"wats/internal/types"
+	"strings"
 )
 
 type MarketData struct {
@@ -16,51 +14,15 @@ type MarketData struct {
 }
 
 func NewMarketData(symbol string) *MarketData {
-	return &MarketData{
-		symbol: symbol,
-	}
+	return &MarketData{symbol: symbol}
 }
 
-func (m *MarketData) GetCandles(year, month, day int) ([]*types.Candle, error) {
-	i := "monthly"
-	d := fmt.Sprintf("%d-%02d", year, month)
-	if day > 0 {
-		i = "daily"
-		d = fmt.Sprintf("%s-%02d", d, day)
-	}
-
+func (m *MarketData) DownloadFile(year, month, day int) (filePath string, err error) {
 	url := fmt.Sprintf(
-		"https://data.binance.vision/data/futures/um/%s/klines/%s/1m/%s-1m-%s.zip",
-		i, m.symbol, m.symbol, d,
+		"https://data.binance.vision/data/futures/um/daily/klines/%s/1m/%s-1m-%d-%02d-%02d.zip",
+		m.symbol, m.symbol, year, month, day,
 	)
 
-	fp, err := downloadFile(url)
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(fp)
-
-	zrc, err := zip.OpenReader(fp)
-	if err != nil {
-		return nil, err
-	}
-	defer zrc.Close()
-
-	irc, err := zrc.File[0].Open()
-	if err != nil {
-		return nil, err
-	}
-	defer irc.Close()
-
-	cds, err := m.parseCSV(irc)
-	if err != nil {
-		return nil, err
-	}
-
-	return cds, nil
-}
-
-func downloadFile(url string) (filePath string, err error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", err
@@ -75,6 +37,7 @@ func downloadFile(url string) (filePath string, err error) {
 	if err != nil {
 		return "", err
 	}
+	tfPath := tf.Name()
 
 	_, err = io.Copy(tf, resp.Body)
 	if err != nil {
@@ -82,53 +45,34 @@ func downloadFile(url string) (filePath string, err error) {
 		return "", err
 	}
 	tf.Close()
+	defer os.Remove(tfPath)
 
-	return tf.Name(), nil
-}
+	zrc, err := zip.OpenReader(tfPath)
+	if err != nil {
+		return "", err
+	}
+	defer zrc.Close()
 
-func (m *MarketData) parseCSV(rc io.ReadCloser) ([]*types.Candle, error) {
-	r := csv.NewReader(rc)
-	r.FieldsPerRecord = -1 // 레코드당 필드 수 제한 두지 않음
+	irc, err := zrc.File[0].Open()
+	if err != nil {
+		return "", err
+	}
+	defer irc.Close()
 
-	var cds []*types.Candle
+	sUrl := strings.Split(url, "/")
+	fileName := strings.Split(sUrl[len(sUrl)-1], ".")[0] + ".csv"
 
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		openTime, _ := strconv.ParseInt(record[0], 10, 64)
-		open, _ := strconv.ParseFloat(record[1], 64)
-		high, _ := strconv.ParseFloat(record[2], 64)
-		low, _ := strconv.ParseFloat(record[3], 64)
-		close, _ := strconv.ParseFloat(record[4], 64)
-		volume, _ := strconv.ParseFloat(record[5], 64)
-		closeTime, _ := strconv.ParseInt(record[6], 10, 64)
-		quoteVolume, _ := strconv.ParseFloat(record[7], 64)
-		count, _ := strconv.Atoi(record[8])
-		takerBuyVolume, _ := strconv.ParseFloat(record[9], 64)
-		takerBuyQuoteVolume, _ := strconv.ParseFloat(record[10], 64)
-
-		cds = append(cds, &types.Candle{
-			Symbol:              m.symbol,
-			OpenTime:            openTime,
-			Open:                open,
-			High:                high,
-			Low:                 low,
-			Close:               close,
-			Volume:              volume,
-			CloseTime:           closeTime,
-			QuoteVolume:         quoteVolume,
-			Count:               count,
-			TakerBuyVolume:      takerBuyVolume,
-			TakerBuyQuoteVolume: takerBuyQuoteVolume,
-			Closed:              true,
-		})
+	f, err := os.Create(fileName)
+	if err != nil {
+		return "", err
 	}
 
-	return cds, nil
+	_, err = io.Copy(f, irc)
+	if err != nil {
+		f.Close()
+		return "", err
+	}
+	f.Close()
+
+	return f.Name(), nil
 }
