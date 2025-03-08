cc44e4ef018c13a8e6ab2f1bf55a6e5b6cb59c01
waik1ki <arbutus@outlook.kr>
2025-03-08 23:10:35 +0900

    feat: 바이낸스 Kline API 데이터 저장 로직 추가

--- internal/trading/trader/trader.go
+++ internal/trading/trader/trader.go
@@ -3,12 +3,17 @@ package trader
 import (
 	"context"
 	"log"
+	"strconv"
 	"sync"
 	"time"
 	"wats/internal/database"
 	"wats/internal/trading/analyzer"
 	"wats/internal/trading/chart"
 	"wats/internal/trading/market"
+	"wats/internal/types"
+
+	"github.com/adshao/go-binance/v2"
+	"github.com/adshao/go-binance/v2/futures"
 )
 
 type Trader struct {
@@ -83,7 +88,7 @@ func (t *Trader) syncCandleData() {
 				default:
 					fp, err := m.DownloadFile(date.Year(), int(date.Month()), date.Day())
 					if err != nil {
-						log.Printf("%d-%02d-%02d 데이터 다운로드 실패: %w", date.Year(), int(date.Month()), date.Day(), err)
+						log.Printf("%d-%02d-%02d 데이터 다운로드 실패: %s", date.Year(), int(date.Month()), date.Day(), err)
 						return
 					}
 					fpCh <- fp
@@ -119,5 +124,87 @@ func (t *Trader) syncCandleData() {
 		}
 
 		wgDB.Wait()
+	}
+
+	client := binance.NewFuturesClient(
+		"",
+		"",
+	)
+
+	var cds []*types.Candle
+
+	lc = t.db.GetLatestCandle(t.symbol)
+	startTime := lc.CloseTime
+
+	for {
+		klines, err := client.NewKlinesService().
+			// cfg 파일로 분리 필요 (임시)
+			Symbol("ETHUSDT").
+			Interval("1m").
+			StartTime(startTime).
+			Limit(1500).
+			Do(t.ctx)
+		if err != nil {
+			log.Println("API 요청 실패 : ", err)
+			break
+		}
+
+		isBreak := false
+
+		for i, k := range klines {
+			c := convertKlineToCandle(k)
+
+			if i == len(klines)-1 {
+				lcSecond := time.UnixMilli(int64(lc.CloseTime)).Second()
+				cSecond := time.UnixMilli(int64(c.CloseTime)).Second()
+
+				if lcSecond == cSecond {
+					isBreak = true
+				} else {
+					startTime = lc.CloseTime
+					cds = append(cds, c)
+					lc = c
+				}
+			} else {
+				cds = append(cds, c)
+			}
+		}
+
+		if isBreak {
+			break
+		}
+	}
+
+	err := t.db.SaveCandles(t.symbol, cds)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("sync 작업 종료.")
+}
+
+func convertKlineToCandle(k *futures.Kline) *types.Candle {
+	open, _ := strconv.ParseFloat(k.Open, 64)
+	high, _ := strconv.ParseFloat(k.High, 64)
+	low, _ := strconv.ParseFloat(k.Low, 64)
+	close, _ := strconv.ParseFloat(k.Close, 64)
+	volume, _ := strconv.ParseFloat(k.Volume, 64)
+	quoteVolume, _ := strconv.ParseFloat(k.QuoteAssetVolume, 64)
+	takerBuyVolume, _ := strconv.ParseFloat(k.TakerBuyBaseAssetVolume, 64)
+	takerBuyQuoteVolume, _ := strconv.ParseFloat(k.TakerBuyQuoteAssetVolume, 64)
+
+	return &types.Candle{
+		Symbol:              "",
+		OpenTime:            k.OpenTime,
+		Open:                open,
+		High:                high,
+		Low:                 low,
+		Close:               close,
+		Volume:              volume,
+		CloseTime:           k.CloseTime,
+		QuoteVolume:         quoteVolume,
+		Count:               int(k.TradeNum),
+		TakerBuyVolume:      takerBuyVolume,
+		TakerBuyQuoteVolume: takerBuyQuoteVolume,
 	}
 }
