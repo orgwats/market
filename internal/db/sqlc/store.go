e1ba6f7ac38e4c9d9e94e21c1870dc9a2f3cf26e
waik1ki <arbutus@outlook.kr>
2025-03-19 19:09:10 +0900

    feat db store 파일 추가

---
+++ internal/db/sqlc/store.go
@@ -0,0 +1,92 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"strings"
+)
+
+type Store interface {
+	Querier
+	SaveCandles(ctx context.Context, symbol string, cds []*Candle) error
+	SaveCandlesFromCSV(ctx context.Context, symbol, filePath string) error
+}
+
+type SQLStore struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *SQLStore {
+	return &SQLStore{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (s *SQLStore) SaveCandlesFromCSV(ctx context.Context, symbol, filePath string) error {
+	query := fmt.Sprintf(`
+		LOAD DATA LOCAL INFILE './%s'
+		INTO TABLE candle
+		FIELDS TERMINATED BY ','
+		LINES TERMINATED BY '\n'
+		IGNORE 1 LINES
+		(open_time, open, high, low, close, volume, close_time, quote_volume, count, taker_buy_volume, taker_buy_quote_volume, @ignore)
+		SET symbol = '%s'
+	`, filePath, symbol)
+
+	_, err := s.db.ExecContext(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *SQLStore) SaveCandles(ctx context.Context, symbol string, candles []*Candle) error {
+	const batchSize = 1000
+	for start := 0; start < len(candles); start += batchSize {
+		end := start + batchSize
+		if end > len(candles) {
+			end = len(candles)
+		}
+
+		query, args := generateBulkInsertQuery(symbol, candles[start:end])
+		_, err := s.db.ExecContext(ctx, query, args...)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func generateBulkInsertQuery(symbol string, chunk []*Candle) (query string, arg []interface{}) {
+	var sb strings.Builder
+	sb.WriteString("INSERT INTO candle (symbol, open_time, open, high, low, close, volume, close_time, count, quote_volume, taker_buy_volume, taker_buy_quote_volume) VALUES ")
+
+	placeholders := make([]string, 0, len(chunk))
+	args := make([]interface{}, 0, len(chunk)*12)
+
+	for _, c := range chunk {
+		placeholders = append(placeholders, "(?,?,?,?,?,?,?,?,?,?,?,?)")
+		args = append(args,
+			symbol,
+			c.OpenTime,
+			c.Open,
+			c.High,
+			c.Low,
+			c.Close,
+			c.Volume,
+			c.CloseTime,
+			c.Count,
+			c.QuoteVolume,
+			c.TakerBuyVolume,
+			c.TakerBuyQuoteVolume,
+		)
+	}
+
+	sb.WriteString(strings.Join(placeholders, ","))
+
+	return sb.String(), args
+}
